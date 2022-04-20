bcd838a970d0aa7b6ce1a0b0eb3d2bbfcca19395
WODweqFS <prohnovigor11@gmail.com>
2022-04-20 22:42:48 +0300

    add After

--- src/main/java/aop/aspects/UniversityLoggingAspect.java
+++ src/main/java/aop/aspects/UniversityLoggingAspect.java
@@ -1,10 +1,7 @@
 package aop.aspects;
 
 import aop.Student;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -18,7 +15,9 @@ public class UniversityLoggingAspect {
 //        System.out.println("beforeGetStudentsLoggingAdvice:" +
 //                " логируем получение списка студентов перед методом getStudents()");
 //    }
-//
+
+//   ====================================================================================================
+
 //    @AfterReturning(pointcut = "execution(* getStudents())", returning = "students")
 //    public void afterReturningGetStudentsLoggingAdvice(List<Student> students) {
 //
@@ -35,9 +34,17 @@ public class UniversityLoggingAspect {
 //                " логируем получение списка студентов после работы метода getStudents()");
 //    }
 
-    @AfterThrowing(pointcut = "execution(* getStudents())", throwing = "exception")
-    public void afterThrowingGetStudentsLoggingAdvice(Throwable exception){
-        System.out.println("afterThrowingGetStudentsLoggingAdvice: логируем выброс исключения " + exception);
-    }
+//   ====================================================================================================
 
+//    @AfterThrowing(pointcut = "execution(* getStudents())", throwing = "exception")
+//    public void afterThrowingGetStudentsLoggingAdvice(Throwable exception){
+//        System.out.println("afterThrowingGetStudentsLoggingAdvice: логируем выброс исключения " + exception);
+//    }
+
+//    ====================================================================================================
+
+    @After("execution(* getStudents())")
+    public void afterGetStudentsLoggingAdvice(){
+        System.out.println("afterGetStudentsLoggingAdvice: логируем окончание работы метода или выброса ислючения");
+    }
 }
